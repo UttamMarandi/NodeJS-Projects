05a0adcd5aca51f7bc82668f1ff1609059df5333
Uttam Marandi <uttammarandi007@gmail.com>
2021-11-29 20:53:08 +0530

    JWT basics - install , setting up routes

---
+++ JWT Basics/controllers/main.js
@@ -0,0 +1,23 @@
+const CustomAPIError = require("../errors/custom-error");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+  res.send("Fake Login/Register/Signup Route");
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msj: `Hello jhon doe`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
+};
+
+module.exports = {
+  login,
+  dashboard,
+};
